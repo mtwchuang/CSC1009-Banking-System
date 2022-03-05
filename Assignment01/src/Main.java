8be35839c3222a82d14f0e585a206396f9acc24e
Actarnix <85341741+mtwchuang@users.noreply.github.com>
2022-03-05 16:58:28 +0800

    test

--- Assignment01/src/Main.java
+++ Assignment01/src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) throws Exception 
     {
         // clear message
-        System.out.print("\033[H\033[2J");
-        View.V_Login v1 = new View.V_Login();
-        v1.Authentication();
-
+        // System.out.print("\033[H\033[2J");
+        // View.V_Login v1 = new View.V_Login();
+        // v1.Authentication();
+        View.V_Overview ov1 = new View.V_Overview();
+        ov1.userActions();
         
     }
 }
--- Assignment01/src/View/V_Overview.java
+++ Assignment01/src/View/V_Overview.java
@@ -2,10 +2,43 @@ package View;
 import java.util.Scanner;
 public class V_Overview 
 {
+    Scanner input = new Scanner(System.in);
     //Action selections
     public void userActions()
     {
+        System.out.print("----------------------------Overview----------------------------\n");
+        System.out.println("\n1. Check Account Balance\n2. Check Account Information");
+        System.out.println("3. Money Transfer\n4. Deposit Money\n5. Withdraw Money\n6. Edit Settings");
         // get user input for user actions
+        System.out.print("Enter choice(number): ");
+        int actionChoice = input.nextInt();
+        switch(actionChoice)
+        {
+            case(1):
+            {
+                // call method for balance checking
+            }
+            case(2):
+            {
+                // call method for account info checking
+            }
+            case(3):
+            {
+                // call money transfer
+            }
+            case(4):
+            {
+                // call deposit money
+            }
+            case(5):
+            {
+                // call withdraw money
+            }
+            case(6):
+            {
+                // call edit settings
+            }
+        }
         // method for account information
         // method for balance checking
         // method for money transfer
