79708c54b84282316851983cbff2e131544e4307
Actarnix <85341741+mtwchuang@users.noreply.github.com>
2022-03-05 14:06:16 +0800

    commit

--- Assignment01/src/Main.java
+++ Assignment01/src/Main.java
@@ -8,15 +8,10 @@ import ModelView.Global;
 public class Main {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("SessionID: " + Global.sessionUserID.toString());
-
+        // clear ,essage
         System.out.print("\033[H\033[2J");
-
-        IUserAccount temp = new UserAccount();
-        System.out.println(
-            "newAccID: " + temp.getUserID().toString() + "\n" +
-            "createdBy: " + temp.getCreatedBy().toString() + "\n" + 
-            "createdAt: " + new Date(temp.getCreatedAt()));
+        View.Login v1 = new View.Login();
+        v1.Authentication();
 
         
     }
